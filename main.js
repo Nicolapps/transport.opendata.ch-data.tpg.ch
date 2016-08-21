1d4a6976cf80316606c11cd4662334f032204d02
Nicolapps <webmaster@nicolapps.ch>
2016-08-22 00:21:00 +0200

    Database connection

--- main.js
+++ main.js
@@ -4,15 +4,29 @@
 
 var colors  = require('colors'),
     mysql   = require('mysql'),
-    request = require('request');
+    request = require('request')
 
-console.log('transport.opendata.ch-data.tpg.ch'.cyan.inverse);
+console.log('transport.opendata.ch-data.tpg.ch'.cyan.inverse)
 
 //-------------------------------------
 // Connect to the MySQL database
 //-------------------------------------
 
-// @TODO : MySQL Database connection
+console.log('Connecting to database...')
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'my_db'
+})
+
+connection.connect((error) => {
+  if(error) {
+    console.log('ERROR'.red.inverse + " Cannot connect to database. Please make sure that the database information in main.js is correct.")
+    process.exit();
+  }
+})
 
 //-------------------------------------
 // Fetch GetPhysicalStops from the
