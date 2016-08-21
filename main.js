fe83360b3ea75cdeb4f1a8296604b54460efca9e
Nicolapps <webmaster@nicolapps.ch>
2016-08-22 00:02:02 +0200

    Created main.js file structure

---
+++ main.js
@@ -0,0 +1,22 @@
+// ====================================
+// transport.opendata.ch-data.tpg.ch
+// ====================================
+
+var colors  = require('colors'),
+    mysql   = require('mysql'),
+    request = require('request');
+
+console.log('transport.opendata.ch-data.tpg.ch'.cyan.inverse);
+
+//-------------------------------------
+// Connect to the MySQL database
+//-------------------------------------
+
+// @TODO : MySQL Database connection
+
+//-------------------------------------
+// Fetch GetPhysicalStops from the
+// transports publics genevois API
+//-------------------------------------
+
+// @TODO : Fetch GetPhysicalStops
