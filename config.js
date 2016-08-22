40f2d9d10c1343d63f13128e402d22650413c81a
Nicolapps <webmaster@nicolapps.ch>
2016-08-22 11:10:31 +0200

    Created config.js file

---
+++ config.js
@@ -0,0 +1,17 @@
+var exports = module.exports = {}
+
+//-------------------------------------
+// MySQL database
+//-------------------------------------
+exports.database = {
+  'host': 'localhost',
+  'user': 'root',
+  'password': 'root',
+  'db_name': 'my_db'
+}
+
+//-------------------------------------
+// TPG open data API key
+// (get one at http://data.tpg.ch)
+//-------------------------------------
+exports.apiKey = 'xxxx'
--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 
 var colors  = require('colors'),
     mysql   = require('mysql'),
-    request = require('request')
+    request = require('request'),
+    config  = require('./config')
 
 console.log('transport.opendata.ch-data.tpg.ch'.cyan.inverse)
 
@@ -15,15 +16,15 @@ console.log('transport.opendata.ch-data.tpg.ch'.cyan.inverse)
 console.log('Connecting to database...')
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'my_db'
+  host     : config.database.host,
+  user     : config.database.user,
+  password : config.database.password,
+  database : config.database.db_name
 })
 
 connection.connect((error) => {
   if(error) {
-    console.log('ERROR'.red.inverse + " Cannot connect to database. Please make sure that the database information in main.js is correct.")
+    console.log('ERROR'.red.inverse + " Cannot connect to database. Please make sure that the database information in config.js is correct.")
     process.exit();
   }
 })
